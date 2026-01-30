b94715f41abb48823bc954946c45b35d66e51ca6
AntonDawinan <antondawinan72@gmail.com>
2026-01-30 15:33:44 +0800

    Uploaded py files

---
+++ ListProcessing.py
@@ -0,0 +1,10 @@
+numbers = []
+
+for i in range(5):
+    num = int(input("Input a number: "))
+    numbers.append(num)
+
+numbers.sort(reverse=True)
+
+print("Sorted numbers (highest to lowest):")
+print(numbers)
